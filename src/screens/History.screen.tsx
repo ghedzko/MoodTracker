ad8dd466a1d939415e8ac8916a92a57a2ee8badb
Guillermo Hedzko <ghedzko@gmail.com>
2022-02-18 20:19:33 -0300

    add component type

--- src/screens/History.screen.tsx
+++ src/screens/History.screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export const History = () => {
+export const History: React.FC = () => {
   return (
     <View>
       <Text>History Screen</Text>
