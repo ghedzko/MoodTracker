c899d80f6955dcbd115c54aecc82222f06b4272c
Guillermo Hedzko <ghedzko@gmail.com>
2022-02-23 23:03:27 -0300

    add screens

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { StyleSheet, View, Text } from 'react-native';
+import { MoodPicker } from '../components/MoodPicker';
+import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
+import { MoodItemRow } from '../components/MoodItemRow';
 export const Home: React.FC = () => {
+  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
+
+  const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
+    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+  }, []);
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <MoodPicker onSelect={handleSelectMood} />
+      {moodList.map(item => (
+        <MoodItemRow key={item.timestamp} item={item} />
+      ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
