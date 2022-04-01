90d8b381518978f0c5211047d88905e96bf87ba2
Guillermo Hedzko <ghedzko@gmail.com>
2022-04-01 11:20:46 -0300

    adding images , splash screen & graph

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useAppContext } from '../App.provider';
+import groupBy from 'lodash/groupBy';
+import { VictoryPie, VictoryTheme } from 'victory-native';
 
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
   return (
     <View>
       <Text>Analytics Screen</Text>
+      <VictoryPie data={data} theme={VictoryTheme.material} />
     </View>
   );
 };
